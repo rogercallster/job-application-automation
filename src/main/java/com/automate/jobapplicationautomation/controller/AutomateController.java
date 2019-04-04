d8e6081c90bb637d0dac1a11bf1d8531e33ddb83
ansaran <ansaran@vmware.com>
2019-04-03 19:58:48 -0700

    commit got FILE upload working

--- src/main/java/com/automate/jobapplicationautomation/controller/AutomateController.java
+++ src/main/java/com/automate/jobapplicationautomation/controller/AutomateController.java
@@ -1,30 +1,15 @@
 package com.automate.jobapplicationautomation.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.automate.jobapplicationautomation.StorageService;
-
 @Controller
 public class AutomateController {
-    private StorageService storageService;
 
 //    @Autowired
 //    public AutomateController(StorageService storageService) {
@@ -55,7 +40,7 @@ public class AutomateController {
     }
 
     @PostMapping(value = "/upload")
-    public void upload(@RequestParam("cv") MultipartFile file,
+    public void upload(@RequestParam("cv") MultipartFile file, @RequestParam("cover") MultipartFile cover,
             RedirectAttributes redirectAttributes){
 
         System.out.println(file);
