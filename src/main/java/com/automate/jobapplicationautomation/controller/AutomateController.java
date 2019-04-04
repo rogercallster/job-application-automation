6540f14849e84f03a969f96b0fe7aac7decdf96b
ansaran <ansaran@vmware.com>
2019-04-03 19:35:50 -0700

    commit 4

--- src/main/java/com/automate/jobapplicationautomation/controller/AutomateController.java
+++ src/main/java/com/automate/jobapplicationautomation/controller/AutomateController.java
@@ -7,18 +7,31 @@ import javax.validation.constraints.NotNull;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.automate.jobapplicationautomation.StorageService;
 
 @Controller
 public class AutomateController {
+    private StorageService storageService;
+
+//    @Autowired
+//    public AutomateController(StorageService storageService) {
+//        this.storageService = storageService;
+//    }
+
+
     @PostMapping(value = "/user", consumes = "application/json")
 //            consumes = {"multipart/form-data"})
     @ResponseBody
@@ -40,5 +53,19 @@ public class AutomateController {
         return "<h1>thanks</h1>";
 
     }
+
+    @PostMapping(value = "/upload")
+    public void upload(@RequestParam("cv") MultipartFile file,
+            RedirectAttributes redirectAttributes){
+
+        System.out.println(file);
+
+//        storageService.store(file);
+//        redirectAttributes.addFlashAttribute("message",
+//                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+
+    }
+
 
 }
